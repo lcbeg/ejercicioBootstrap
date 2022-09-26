c0ffbcddd3a9aaa22722d276c64508674d260caa
Clari Betancourt <lcbeg@ier.unam.mx>
2022-09-26 11:01:49 -0500

    feat: script externo

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("generation").style.color = "grey";
+
+function cambiarColor(){
+// console.log("Hola!, sí funciona");
+document.getElementById("generation").style.color = "pink";
+}
