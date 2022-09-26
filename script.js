96f0a57af27754c524b56c044291a52d732f71a8
Clari Betancourt <lcbeg@ier.unam.mx>
2022-09-26 14:19:20 -0500

    feat: carousel

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function cambiarColor(){
 // console.log("Hola!, sí funciona");
 document.getElementById("generation").style.color = "pink";
 }
+
+function sumaDecimal(numero1,numero2,decimales){
+    let decimales1 = numero1%1;
+    let decimales2= numero2%1;
+
+    let factor = 10**Math.max(decimales1,decimales2);
+    
+    return ((numero1*factor+numero2*factor)/factor).toFixed(decimales);
+
+}
+
